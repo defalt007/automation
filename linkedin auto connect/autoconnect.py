f81f31cb39d102f99094a32e1d055bbdfbb2e948
maniac <sm.cse17@gmail.com>
2020-11-24 17:16:31 +0530

    Add exception for import error

--- linkedin auto connect/autoconnect.py
+++ linkedin auto connect/autoconnect.py
@@ -1,10 +1,15 @@
 #Automatic connection request sender to the search results (LinkedIn)
 #as a fallsafe if script goes unresponsive, take your cursor to anycorner of the screen
-import pyautogui
-import time
+import sys, subprocess
+try:
+    import time
+    import pyautogui
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyautogui"])
+    import pyautogui
 
-message = '''Hi, Hope you are doing great. 
-I would love to add you in my professional network. 
+message = '''Hi, Hope you are doing great.
+I would love to add you in my professional network.
 Thank you'''
 
 while True:
@@ -20,8 +25,8 @@ while True:
 
     sendbutpoint = pyautogui.locateCenterOnScreen('sendbut.png', confidence =0.8) #locate the send invitation button
     pyautogui.click(sendbutpoint) #click on send invitation button
-    
-    time.sleep(1)   #a one second delay just so that linkedin does not consider you a bot spitting connect requests 
-                    #also helps in the fallsafe  
+
+    time.sleep(1)   #a one second delay just so that linkedin does not consider you a bot spitting connect requests
+                    #also helps in the fallsafe
 
 #defalt007
